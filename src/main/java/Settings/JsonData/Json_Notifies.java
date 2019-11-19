e861e4e7a9e72f264d64d0dec3e656311740f74c
Emil Czerski <emil.czerski@l4a-soft.com>
2019-11-19 22:29:53 +0100

    prepare for Selenium GRID

--- src/main/java/Settings/JsonData/Json_Notifies.java
+++ src/main/java/Settings/JsonData/Json_Notifies.java
@@ -1,4 +1,26 @@
 package Settings.JsonData;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Json_Notifies {
+
+    public static String register_success_notify;
+
+    public static void parseJson() {
+        JSONParser parser = new JSONParser();
+        try {
+            Object object = parser.parse(new FileReader(System.getProperty("user.dir")+"/src/resources/Notifications.json"));
+            JSONObject jsonObject = (JSONObject) object;
+            register_success_notify = (String) jsonObject.get("register_success_notify");
+
+
+        } catch (ParseException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
